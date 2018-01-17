88449b15f73848bae896b780173e71f0fead1809
Maximilian Stroh <Hisako1337@gmail.com>
2018-01-17 20:57:37 +0100

    buttons -> links

---
+++ frontend/components/HeaderNavigation.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import {} from "react-bootstrap";
+
+export default ({ children }) => (
+  <p>stuff</p>
+)
--- frontend/pages/hello.tsx
+++ frontend/pages/hello.tsx
@@ -1,23 +1,31 @@
 import { Page } from "@seagull-js/seagull";
 import * as React from "react";
 import { Button, Jumbotron } from "react-bootstrap";
+import { Link } from 'react-router-dom'
 
 // the (stateful) component for the page with type checking
 export default class HelloPage extends Page<{}, {}> {
   path = "/";
 
   render() {
-    return (
-      <div>
+    return <div>
         <Jumbotron>
           <h1>Seagull Framework Logo</h1>
-          <p>Featherweight Zero-Config Typesafe Isomorphic Cloud Framework.</p>
           <p>
-            <Button bsStyle="primary">Learn more</Button>
-            <Button bsStyle="default">Quickstart</Button>
+            Featherweight Zero-Config Typesafe Isomorphic Cloud Framework.
+          </p>
+          <p>
+            <Link className="btn btn-primary" to={"/introduction"}>
+              Introduction
+            </Link>
+            <Link className="btn btn-default" to={"/quickstart"}>
+              Quickstart
+            </Link>
+            <Link className="btn btn-default" to={"/features"}>
+              Features
+            </Link>
           </p>
         </Jumbotron>
-      </div>
-    );
+      </div>;
   }
 }
