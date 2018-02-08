f1d3881457d69e24c6e32c8ca771fb7344a700cc
Maximilian Stroh <Hisako1337@gmail.com>
2018-02-08 22:06:43 +0100

    added line numbers, eliminated ugly whitespace/blank lines

--- frontend/components/CodeSnippet.tsx
+++ frontend/components/CodeSnippet.tsx
@@ -3,7 +3,7 @@ import SyntaxHighlighter from 'react-syntax-highlighter/prism'
 import { okaidia as theme } from 'react-syntax-highlighter/styles/prism'
 
 export default ({ code }) => (
-  <SyntaxHighlighter language="typescript" style={theme}>
-    {code}
+  <SyntaxHighlighter language="typescript" style={theme} showLineNumbers="true">
+    {code.trim()}
   </SyntaxHighlighter>
 )
