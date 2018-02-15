7e3cbe13dea6c5b2cc4d6f262b52ce0ca10ff981
Maximilian Stroh <Hisako1337@gmail.com>
2018-02-15 21:00:08 +0100

    prettier

--- frontend/layout.tsx
+++ frontend/layout.tsx
@@ -1,19 +1,19 @@
-import * as React from "react";
-import {} from "react-bootstrap";
+import * as React from 'react'
+import {} from 'react-bootstrap'
 import { Favicons } from '@seagull/core'
 // this is what your index.html looks like
 export default function Layout({ children }) {
-  return <html>
+  return (
+    <html>
       <head>
         <title>seagull-homepage</title>
         <Favicons favicons={['favicon.ico']} />
         <link rel="stylesheet" href="/assets/bootstrap.min.css" />
       </head>
       <body>
-        <div id="root">
-          {children}
-        </div>
+        <div id="root">{children}</div>
         <script src="/assets/bundle.js" />
       </body>
-    </html>;
+    </html>
+  )
 }
