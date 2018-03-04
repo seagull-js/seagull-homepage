a021b08b40d2d1697223da716a83397f46c241b2
Tom Jaster <tom.jaster@gecko.de>
2018-03-04 12:30:45 +0100

    updated to new seagull version

--- backend/api/Frontend.ts
+++ backend/api/Frontend.ts
@@ -1,9 +1,6 @@
 // library imports
-import { API, Request, Response, Routing } from '@seagull/core'
+import { API, Request, Response, Routing, Document } from '@seagull/core'
 import { renderToString } from 'react-dom/server'
-
-// configuration imports
-import layout from '../../frontend/layout'
 
 // Server Side Rendering for the frontend
 export default class Frontend extends API {
@@ -16,7 +13,7 @@ export default class Frontend extends API {
       await page.componentDidMount()
     }
     const content = renderToString(appRouter.load())
-    const html = renderToString(layout({ content }))
+    const html = renderToString(Document({ content }))
     return this.html(html)
   }
 }
--- frontend/components/PageWrapper.tsx
+++ frontend/components/PageWrapper.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
 import { withRouter } from 'react-router'
 import Block from './Block'
+import { Meta } from '@seagull/core'
 import FooterNavigation from './FooterNavigation'
 import HeaderNavigation from './HeaderNavigation'
+
 
 export interface IProps {
   match: any
@@ -10,9 +12,15 @@ export interface IProps {
   history: any
   children: any
 }
+import { Helmet } from 'react-helmet'
 
 const Wrapper = (props: IProps) => (
   <>
+    <Meta title="Seagull">
+      <link rel="stylesheet" href="/assets/bootstrap.min.css" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Meta>
+
     {props.location.pathname === '/' ? <></> : <HeaderNavigation />}
     <div className="">{props.children}</div>
     <FooterNavigation />
